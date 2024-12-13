82d15b0dae68a6fe2c9ea59c66fd6bdc8bb6192d
Dishasak <dishasakhalkar500@gmail.com>
2024-12-13 21:56:22 +0530

    usedcase3

--- snake_ladder.py
+++ snake_ladder.py
@@ -1,7 +1,22 @@
-#usedcase2
+#usedcase3 
+def moving(value, position, dice_roll):
+    match value:
+        case 1: 
+            return position, "NoPlay"
+        case 2:  
+            position += dice_roll
+            return position, "Ladder"
+        case 3: 
+            position -= dice_roll
+            return position, "Snake"
+        case _:
+            return position, "Invalid move"
 
-import random
-dice_roll = random.randint(1,6)
+while position < target:
+    dice_roll = random.randint(1, 6)  
+    count += 1
+   
+    event = random.randint(1, 3)
+    position, event_name = moving(event, position, dice_roll)
 
-print(dice_roll)
 
